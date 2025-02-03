e5e9f7d6a3f3d921b565a42ab254d61e8ead504d
aBgAmeuR <a.josset@yahoo.com>
2025-02-03 15:42:30 +0100

    feat: update service worker source path in configuration

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import { NextConfig } from 'next';
 const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
   // use something else that works, such as "service-worker/index.ts".
-  swSrc: 'app/sw.ts',
+  swSrc: 'src/app/sw.ts',
   swDest: 'public/sw.js',
 });
 
