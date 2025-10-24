d5f3ce94996fe77206270869dbbb341ea3123877
Antoine <a.josset@yahoo.com>
2025-10-24 23:55:32 +0200

    chore: tsdown config file

--- packages/db/tsdown.config.ts
+++ packages/db/tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-	entry: "src/**/*.ts",
-	sourcemap: true,
-	dts: true,
+  entry: "*.ts",
+  sourcemap: true,
+  dts: true,
 });
